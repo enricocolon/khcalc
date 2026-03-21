698b3fa2558d3d55fe33145960b632b1a29da718
enricocolon <enricocolon@gmail.com>
2026-03-20 21:01:29 -0400

    work on methods for braids

--- braid.py
+++ braid.py
@@ -8,24 +8,30 @@ class BraidElem:
     def __init__(self, n, word=()):
         self.n = n
         self.word = tuple(word)
+        self.alphabet = set{range(-self.n-1,self.n)}-{0} #alphabet for braid generators/their inverses.
+        #i represents (in zero-indexing), a (signed) crossing between strand i-1 and i.
 
         for letter in self.word:
-            if not isinstance(letter, int) or letter < 1 or letter >= n:
+            if letter not in self.alphabet:
                 raise ValueError(f"Invalid letter {letter} in braid word for B_{n}")
 
     def __repr__(self):
         return f"BraidWord({self.n},{self.word})"
 
-    def __str__(self):
-        pass
+    def __str__(self): #replace with better word printing
+        return f"Br_{self.n}({self.word})"
 
-    def __eq__(self):
-        pass
+    def __eq__(self, other):
+        return isinstance(other, BraidElem) and self.n == other.n and self.word == other.word
 
     def copy(self):
-        pass
+        return BraidElem(self.n, self.word)
 
     def __mul__(self, other):
+        '''
+        input: self, other: braid elements
+        output: their concatenation other(self).
+        '''
         pass
 
     def inverse(self):
