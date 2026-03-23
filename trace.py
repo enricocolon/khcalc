70e2e6674d2ae38e59eb1b259a96416bfd0b555a
enricocolon <enricocolon@gmail.com>
2026-03-23 11:55:32 -0400

    began implementing ocneanu trace

--- trace.py
+++ trace.py
@@ -5,11 +5,19 @@ from braid import BraidElem
 from hecke import HeckeElem
 from poly import LaurentPoly
 
-def hecke_emb():
-    pass
-
-def ocneanu_trace():
-    pass
+def ocneanu_trace(n, hecke_elem):
+    '''
+    input: n, a hecke element with terms in S_n and varset ("q", "a")
+    output: the trace, a Laurent polynomial in q and a
+    '''
+    if n < 0:
+        raise ValueError(f"H_{n} not a valid Hecke algebra")
+    if n == 0:
+        return LaurentPoly.one(("q", "a"))
+    else:
+        term = 0
+        new_hecke = 0
+        return term*ocneanu_trace(n-1, new_hecke)
 
 
 
