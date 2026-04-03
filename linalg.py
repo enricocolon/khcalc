c3860c802d51fd8f025063459775a36bec6c0357
enricocolon <enricocolon@gmail.com>
2026-04-03 13:17:39 -0400

    renamed matrix

--- linalg.py
+++ linalg.py
@@ -14,6 +14,27 @@ class FreeModuleElem:
     def __init__(self):
         raise NotImplementedError
 
-class Matrix:
-    def __init__(self):
-        raise NotImplementedError
+class SparseMatrix:
+    def __init__(self, ring, data):
+        '''
+        input:
+            ring: (int, fraction, LaurentPoly, LaurentFrac)
+            data: A dictionary whose keys are pairs (i,j) of nonnegative ints and values are elements of ring in
+            the entry (i,j).
+        output: a SparseMatrix object
+        '''
+        self.ring = ring
+        self.data = dict()
+
+        for key, val in data.items():
+            if not (isinstance(key, tuple) and len(key) == 2 and all(isinstance(k, int) and k >= 0 for k in key)):
+                raise ValueError(f"{key} is not a valid key. Keys must be pairs of nonnegative integers.")
+            if not isinstance(val, ring):
+                raise ValueError(f"{val} is not an element of {ring}.")
+            if val != ring(0):
+                self.data[key] = val
+
+        self._prune()
+
+    def _prune(self):
+        self.data = {key: val for key, val in self.data.items() if val != self.ring(0)}
